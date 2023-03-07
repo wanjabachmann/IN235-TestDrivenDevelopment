0408465a45a7d28c2c76c3ced67a9697dd3e0c1e
wanjabachmann <wanja.bachmann@hotmail.com>
2023-03-07 18:01:26 +0100

    initial commit

---
+++ demo/README.md
@@ -0,0 +1,3 @@
+# Project ch.hftm/demo
+
+Build the project and run all tests with `./mvnw package` or `mvnw.cmd package` for Windows.
---
+++ demo/src/main/java/ch/hftm/Main.java
@@ -0,0 +1,8 @@
+package ch.hftm;
+
+public class Main {
+
+  public String format(String input) {
+    return input.toUpperCase();
+  }
+}
